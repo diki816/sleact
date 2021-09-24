4b4c45e85d5ddc7becc4d500361c70881081af2a
diki816 <dikisun@gmail.com>
2021-09-24 19:21:56 +0900

    gram folder structure

--- aclecture/layouts/App.tsx
+++ aclecture/layouts/App.tsx
@@ -1,7 +1,15 @@
 import React  from "react";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-    return <div> Plase, Wait </div>;
-};
+    // return <div> Plase, Wait!!! </div>;
+    return <Switch>
+        <Redirect exact path="/" to="/login" />
+        <Route path="/login" component={LogIn} />
+        <Route path="/signUp" component={SignUp} />
+    </Switch>
 
+};
 export default App;
